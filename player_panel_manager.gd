6f92d626eaf8878088b0e740f13d08706ca69771
Salt <rosstheartist23@gmail.com>
2024-12-16 02:17:28 -0400

    wooo, im doing pretty good i thinks

--- player_panel_manager.gd
+++ player_panel_manager.gd
@@ -4,21 +4,11 @@ extends Node
 @onready var atk_status: Label = %"ATK Status"
 @onready var def_status: Label = %"DEF Status"
 @onready var dmg_test_label: Label = %"dmg test label"
-@onready var display_player_name: Label = %DisplayPlayerName
-@onready var enter_player_name: LineEdit = %EnterPlayerName
 
 var health := 100.0
 var atk := 100.0
 var def := 100.0
 var dmg = 0.0
-var username = " "
-
-func name_the_player():
-	if Input.is_key_pressed(KEY_ENTER):
-		username = enter_player_name.text
-
-func welcome_dear(): 
-	display_player_name.text = "Player " + str(username)
 
 func _10_dmg(): # this one's incomplete
 	health -= 10
--- uitst.gd
+++ uitst.gd
@@ -1,8 +1,16 @@
 extends Node
 
 @onready var player_panel_manager: Node = %PlayerPanelManager
+@onready var display_player_name: Label = %DisplayPlayerName
+@onready var enter_player_name: LineEdit = %EnterPlayerName
 
+var username = " "
 
 func _input(_event: InputEvent) -> void:
 		if Input.is_action_pressed("Strike"):
 			player_panel_manager.dmg_sent()
+
+
+func _on_enter_player_name_text_changed(new_text: String) -> void:
+	username = enter_player_name.text
+	display_player_name.text = "Player " + str(username)
