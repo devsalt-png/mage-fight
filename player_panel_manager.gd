a65bbd116b2952c0f02c8f94b3eba8940e4a8eac
Salt <rosstheartist23@gmail.com>
2024-12-16 01:09:55 -0400

    Do not create a subscene if ANYTHING that will be in said subscene has a script. Godot will cry.

--- player_panel_manager.gd
+++ player_panel_manager.gd
@@ -1,9 +1,15 @@
 extends Node
 
 @onready var health_status: Label = %"Health Status"
-var health := 100
+@onready var atk_status: Label = %"ATK Status"
+@onready var def_status: Label = %"DEF Status"
+
+var health := 100.0
+var atk := 100.0
+var def := 100.0
 
 func _10_dmg():
 	health -= 10
-	print(health)
+	if health <= 0:
+		health = 0
 	health_status.text = "Health: " + str(health)
--- uitst.gd
+++ uitst.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var player_panel_manager: Node = %PlayerPanelManager
 
-func _input(event: InputEvent) -> void:
+
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_pressed("Strike"):
 		player_panel_manager._10_dmg()
+	# THIS FUNCTION IS A TEST, DO NOT USE THIS IN THE GAME.
