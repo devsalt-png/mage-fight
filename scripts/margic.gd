cc10c0ea4dff766c27deda58204b1ecc0c2045ef
Salt <rosstheartist23@gmail.com>
2024-12-17 16:59:33 -0400

    Let's hope I remembered the right directory

--- scripts/margic.gd
+++ scripts/margic.gd
@@ -11,6 +11,9 @@ func _ready():
 	global_rotation = spawn_rot
 	
 func _physics_process(delta):
-	velocity = Vector2(-SPEED,0).rotated(0)
+	velocity = Vector2(SPEED,0).rotated(0)
 	move_and_slide()
-	
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("HITT")
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ func _physics_process(delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
 	var is_attacking = Input.is_action_pressed("Strike")
 	velocity = direction * speed
+	rotation = PI
 	if is_attacking:
 		velocity = direction * 0
 	else:
@@ -18,5 +19,5 @@ func attack():
 	var instance = margic.instantiate()
 	instance.dir = rotation
 	instance.spawn_pos = global_position
-	instance.spawn_rot = rotation
+	instance.spawn_rot = -rotation
 	player.add_child.call_deferred(instance)
